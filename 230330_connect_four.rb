e47b5776f8d50c0b2d4ffd813fdf26f5c7dd281b
Lucas Gruner <lucas.gruner@gmail.com>
2023-03-30 12:07:54 +0200

    draft

---
+++ 230330_connect_four.rb
@@ -0,0 +1,22 @@
+# Connect Four
+# https://www.codewars.com/kata/56882731514ec3ec3d000009/train/ruby
+
+# The grid is 6 row by 7 columns, those being named from A to G.
+# You will receive a list of strings showing the order of the pieces which dropped in columns:
+
+# pieces_position_list = ["A_Red",
+#   "B_Yellow",
+#   "A_Red",
+#   "B_Yellow",
+#   "A_Red",
+#   "B_Yellow",
+#   "G_Red",
+#   "B_Yellow"]
+
+# The list may contain up to 42 moves and shows the order the players are playing.
+# The first player who connects four items of the same color is the winner.
+# You should return "Yellow", "Red" or "Draw" accordingly.
+
+def who_is_winner(pieces_position_list)
+  #le code
+end
