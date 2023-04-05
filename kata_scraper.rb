488d6f2d72205fa6e1a2d57a3f69e051e810caec
Lucas Gruner <lucas.gruner@gmail.com>
2023-04-05 09:52:27 +0200

    made file format dynamic

--- kata_scraper.rb
+++ kata_scraper.rb
@@ -1,10 +1,16 @@
 require 'watir'
 require 'nokogiri'
 
-# TODO: make language dynamic, currently only Ruby
+# URL language and file format correspondence (to be completed)
+languages = {
+  'ruby' => 'rb'
+}
 
 # Get URL from command line argument
 url = ARGV.first
+
+# Assign relevant file format to variable
+language = languages[url.match(/\/(?<lang>\w+)$/)[:lang]]
 
 # Open a browser window silently and go to the URL using Watir
 browser = Watir::Browser.new :chrome, headless: true
@@ -22,11 +28,11 @@ code = ''
 html.search('.CodeMirror-code')[0].search('.CodeMirror-line').each { |line| code += "#{line.text}\n" }
 
 # Create filename from title
-def filename(text)
-  "#{Time.now.strftime('%y%m%d')}_#{text.downcase.scan(/\w+/).join('_')}.rb"
+def filename(text, language)
+  "#{Time.now.strftime('%y%m%d')}_#{text.downcase.scan(/\w+/).join('_')}.#{language}"
 end
 
 # Write file (or append if existing, safety measure)
-file = File.new(filename(title), 'a')
+file = File.new(filename(title, language), 'a')
 file.write("#{title}\n#{url}\n\n#{description}\n\n#{code}")
 file.close
